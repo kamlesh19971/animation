aec69cc064fe28d4df528a47fea23808e0f2ba9e
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-18 16:08:30 +0530

    multi-step animation

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -7,12 +7,19 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./zippy.component.css'],
   animations: [
     trigger('expandCollapse', [
-      state('collapsed', style({ height: 0, paddingTop: 0, paddingBottom: 0, overflow: 'hidden' })),
+      state('collapsed', style({
+        height: 0,
+        paddingTop: 0,
+        paddingBottom: 0,
+        overflow: 'hidden',
+        opacity: 0
+      })),
 
       // state('expanded', style({ height: '*', padding: '*', overflow: 'auto' })),
 
       transition('collapsed => expanded', [
-        animate("300ms ease-out")
+        animate("300ms ease-out", style({ height: '*', paddingTop: '*', paddingBottom: '*' })),
+        animate('1s', style({ opacity: 1 }))
       ]),
 
       transition('expanded => collapsed', [
