415dc09e3875ab6a53501caa5cdf9d0adc2d5361
kamleshthavani12345 <kamleshthavani12345@gmail.com>
2022-05-18 15:55:38 +0530

    working with custom states

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,9 +1,25 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({ height: 0, paddingTop: 0, paddingBottom: 0, overflow: 'hidden' })),
+
+      // state('expanded', style({ height: '*', padding: '*', overflow: 'auto' })),
+
+      transition('collapsed => expanded', [
+        animate("300ms ease-out")
+      ]),
+
+      transition('expanded => collapsed', [
+        animate("300ms ease-in")
+      ])
+    ])
+  ]
 })
 export class ZippyComponent {
   @Input('title') title: string = '';
